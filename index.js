5e93e9147d312b8df7d73b780892b3bb4c6b03ec
Aaron Storck <aaron_storck@ultimatesoftware.com>
2016-02-21 14:53:27 -0500

    Fixed typo and added missing ouro dependency.

--- index.js
+++ index.js
@@ -1,24 +1,25 @@
 /*
 
 
-  sSSs   .S       S.    .S_sSSs      sSSs   .S_sSSs        .S    sSSs
- d%%SP  .SS       SS.  .SS~YS%%b    d%%SP  .SS~YS%%b      .SS   d%%SP
-d%S'    S%S       S%S  S%S   `S%b  d%S'    S%S   `S%b     S%S  d%S'
-S%|     S%S       S%S  S%S    S%S  S%S     S%S    S%S     S%S  S%|
-S&S     S&S       S&S  S%S    d*S  S&S     S%S    d*S     S&S  S&S
-Y&Ss    S&S       S&S  S&S   .S*S  S&S_Ss  S&S   .S*S     S&S  Y&Ss
-`S&&S   S&S       S&S  S&S_sdSSS   S&S~SP  S&S_sdSSS      S&S  `S&&S
-  `S*S  S&S       S&S  S&S~YSSY    S&S     S&S~YSY%b      S&S    `S*S
-   l*S  S*b       d*S  S*S         S*b     S*S   `S%b     d*S     l*S
-  .S*P  S*S.     .S*S  S*S         S*S.    S*S    S%S    .S*S    .S*P
-sSS*S    SSSbs_sdSSS   S*S          SSSbs  S*S    S&S  sdSSS   sSS*S
-YSS'      YSSP~YSSY    S*S           YSSP  S*S    SSS  YSSY    YSS'
-                       SP                  SP
-                       Y                   Y
+  sSSs_sSSs     .S       S.    .S_sSSs      sSSs_sSSs
+ d%%SP~YS%%b   .SS       SS.  .SS~YS%%b    d%%SP~YS%%b
+d%S'     `S%b  S%S       S%S  S%S   `S%b  d%S'     `S%b
+S%S       S%S  S%S       S%S  S%S    S%S  S%S       S%S
+S&S       S&S  S&S       S&S  S%S    d*S  S&S       S&S
+S&S       S&S  S&S       S&S  S&S   .S*S  S&S       S&S
+S&S       S&S  S&S       S&S  S&S_sdSSS   S&S       S&S
+S&S       S&S  S&S       S&S  S&S~YSY%b   S&S       S&S
+S*b       d*S  S*b       d*S  S*S   `S%b  S*b       d*S
+S*S.     .S*S  S*S.     .S*S  S*S    S%S  S*S.     .S*S
+ SSSbs_sdSSS    SSSbs_sdSSS   S*S    S&S   SSSbs_sdSSS
+  YSSP~YSSY      YSSP~YSSY    S*S    SSS    YSSP~YSSY
+                              SP
+                              Y
 
-                  -= Ouro Command Line Interace =-
+
+         -= Ouro Command Line Interace =-
 
 */
 
 module.exports.Cli = require('./lib/cli/class');
-module.exports.Command = require('./lib/comdmand/class');
+module.exports.Command = require('./lib/command/class');
